ae88ca3c93a734cee3bef231fc8099c63ba8c0bb
Mohammad-Keath <m.ghyath.z@gmail.com>
2023-07-10 08:12:26 +0000

    message queue

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # airline-system
 
-[Pull Request](https://github.com/Mohammad-Keath/airline-system/pull/4)
+[Pull Request](https://github.com/Mohammad-Keath/airline-system/pull/5)
 
 ![soket](./socket.io.png)
--- pilot/pilot.js
+++ pilot/pilot.js
@@ -7,14 +7,23 @@ const pilotCon = ioClient.connect(URL)
 const pilotConnection = ioClient.connect(link)
 
 /////////////////////////////////////////////////////////
+pilotCon.emit('get_all')
 pilotCon.on('schedialed',(payload)=>{
     // console.log(`the flight with id ${payload.id} has scadualed`);
-   setTimeout(() => {
-       pilotConnection.emit('took-off',payload)
+    setTimeout(() => {
+       pilotConnection.emit('took-off',(payload))
+   }, 4000);
+})
+pilotCon.on('flight',(payload)=>{
+    console.log(`Sorry i didn't catch this flight ID ${payload.id}`);
+    setTimeout(() => {
+       pilotConnection.emit('took-off',(payload))
    }, 4000);
 })
 pilotCon.on('ArrivedM',(payload)=>{
     console.log(`${payload.id} flight arrived`);
+    pilotCon.emit('delete',(payload))
+
 })
 
 
